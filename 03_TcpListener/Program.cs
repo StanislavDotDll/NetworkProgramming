4d66204625c1d3c7f3f7c3521370b933b13429fe
benvaro <benvaro91@gmail.com>
2020-10-07 14:35:27 +0300

    TcpClient, TcpListener

---
+++ 03_TcpListener/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_TcpListener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TcpClient   - active
+            // TcpListener - passive
+            const int port = 2020;
+            var ip = IPAddress.Parse("127.0.0.1");
+            const int size = 512;
+            TcpListener server = new TcpListener(ip, port);
+            try
+            {
+                server.Start();
+
+                while (true)
+                {
+                    Console.WriteLine("Wait for connection...");
+                    try
+                    {
+                        TcpClient client = server.AcceptTcpClient();
+                        Console.WriteLine("Connected: {0}", client.Client.LocalEndPoint);
+                        NetworkStream stream = client.GetStream();
+
+                        byte[] buffer = new byte[size];
+
+                        int count = 0;
+                        do
+                        {
+                            count += stream.Read(buffer, 0, buffer.Length);
+                        } while (stream.DataAvailable);
+
+                        string data = Encoding.UTF8.GetString(buffer, 0, count);
+                        Console.WriteLine("Got: {0} bytes, data:{1}", count, data);
+                        data = String.Format("Responce: got {0} bytes", count);
+                        stream.Write(Encoding.UTF8.GetBytes(data), 0, data.Length);
+                       // stream.Close();
+                        client.Close(); // Clean resources
+                    }
+                    catch (SocketException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        // throw;
+                    }
+                }
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                server.Stop();
+            }
+        }
+    }
+}
---
+++ 04_TcpClient/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace _04_TcpClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int port = 2020;
+            TcpClient client = new TcpClient();
+            NetworkStream stream = null;
+            const int size = 512;
+            try
+            {
+                bool flag = true;
+                //    client.Connect("127.0.0.1", port);
+                while (flag)
+                {
+                    client = new TcpClient("127.0.0.1", port);
+                    Console.WriteLine("Connected to server {0}", client.Client.RemoteEndPoint);
+                    stream = client.GetStream();
+                    Console.WriteLine("Enter message to send:");
+                    string msg = Console.ReadLine();
+
+                    stream.Write(Encoding.UTF8.GetBytes(msg), 0, msg.Length);
+                    if (msg.Equals("Bye"))
+                        flag = false;
+
+                    byte[] responce = new byte[size];
+                    int count = 0;
+                    do
+                    {
+                         count = stream.Read(responce, 0, responce.Length);
+                    } while (stream.DataAvailable);
+                    Console.WriteLine("Received: {0}", Encoding.UTF8.GetString(responce, 0, count));
+                }
+                stream.Close();
+                client.Close();
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+          
+        }
+    }
+}
